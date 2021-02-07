d31adc10f968a24107773a95de514d6f91e27881
falsycat <me@falsy.cat>
2021-02-07 02:07:22 +0000

    Creates new project.

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path    = require("path");
+const webpack = require("webpack");
+
+const plugins = {
+  htmlWebpack: require("html-webpack-plugin"),
+};
+
+module.exports = {
+  entry: "./frontend/js/main.js",
+  output: {
+    filename: "script.js",
+    path: path.join(__dirname, "public", "static")
+  },
+  plugins: [
+      new plugins.htmlWebpack({
+        template: path.resolve(__dirname, "frontend" , "index.html"),
+        filename: "index.html",
+        inject: "head",
+      }),
+      new webpack.DefinePlugin({
+        RAPIDWOLF_RAPIDAPI_KEY: JSON.stringify(process.env.RAPIDWOLF_RAPIDAPI_KEY),
+      }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: true,
+              importLoaders: 2
+            }
+          },
+          "sass-loader",
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public", "static")
+  },
+  devtool: "cheap-module-source-map",
+};
